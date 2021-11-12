d5f2b1857bdc7343e19d83ee2b0b89588a182a8d
Molly Graham <molly.graham@praxent.com>
2021-11-12 11:08:53 -0600

    Add boilerplate code for button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,1 +1,24 @@
-export {}
+import * as React from 'react';
+import Button from "@mui/material/Button";
+
+interface Props {
+
+  onClick: () => void;
+}
+
+const Button: React.FC<Props> = ({ 
+
+    onClick, 
+  
+  }) => { 
+  return (
+    <button 
+      onClick={onClick}
+     
+    >
+    
+    </button>
+  );
+}
+
+export default Button;
