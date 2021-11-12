d32c90776b04a31588c4b4661d2756d8f2984ee3
Daniela0106 <daniela.barrientos.grisales@gmail.com>
2021-11-12 16:46:29 -0500

    Adding background styles

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar'
-import { IconButton, Toolbar } from '@mui/material'
+import { Toolbar } from '@mui/material'
 import TastyTurkeyText from '../../styles/assets/text_restaurant_name.png'
 
 export const StyledAppBar = () => (
-  <AppBar position="static">
-    <Toolbar>
-      <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-        <img src={TastyTurkeyText} alt="tasty turkey" />
-      </IconButton>
+  <AppBar position="sticky" style={{ background: '#fff', height: '70px', textAlign: 'center' }}>
+    <Toolbar style={{ margin: 'auto', display: 'flex' }}>
+      <img src={TastyTurkeyText} alt="tasty turkey" style={{ height: '50px' }} />
     </Toolbar>
   </AppBar>
 )
